f9e8e199c1788b2b1a0c2faab7a9f28c46b684dc
ivangift <ivangift@gmail.com>
2016-02-05 11:56:11 +0800

    clear cache

--- nikkirec.js
+++ nikkirec.js
@@ -4,6 +4,7 @@ function init() {
     	"<span style='color:red'>增设亚洲服务器，请访问<a href='http://ivangift.github.io/nikkirec'>这个地址</a>以提高速度。</span>");
   }
 }
+// randomly change sth
 
 $(document).ready(function() {
   init();
